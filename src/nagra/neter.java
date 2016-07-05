7df4a54532908fe467b8b5c423436b252835b217
NeterPuneet Singh <neternagra@gmail.com>
2016-07-05 09:27:47 -0700

    update file

---
+++ src/nagra/neter.java
@@ -0,0 +1,25 @@
+package nagra;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class neter {
+	public static WebDriver driver = new FirefoxDriver();
+@Test
+public void testjenkins()
+{
+	
+driver.get("https://www.facebook.com");
+System.out.println(driver.getTitle());
+//driver.findElement(By.xpath("neternagra")).click();
+	
+		
+		}
+		
+	
+}		
+	
+	
+
+
